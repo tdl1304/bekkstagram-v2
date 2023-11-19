8ca50bcb4f95a269de0225c6df179368b87d1f66
Tommy Luu <tommydl@stud.ntnu.no>
2023-11-19 20:01:12 +0100

    elivs stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import images from "./data/images";
+
 function App() {
-  return null;
+  return (
+    <div className="App">
+      <Header />
+      <div className="posts">
+        {images.map((image) => (
+        <Image id={image.id} src={image.url} alt={image.description}/>
+        ))}
+      </div>
+    </div>);
 }
+
+
+function Header() {
+  return (
+    <header>
+      <h1>Bekkstagram</h1>
+    </header>
+  )
+}
+
+function Image(props:any) {
+  return (
+    <img
+      src={props.src}
+      alt={props.alt}
+      className="image"
+    />
+  )
+}
+
+
 
 export default App;
